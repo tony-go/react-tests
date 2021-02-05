07e89dded14858d8bc00879cdfafec80b8f256c4
tony-go <gorez.tony@gmail.com>
2021-02-05 10:58:14 +0100

    feat: add test base

---
+++ src/features/login/index.test.js
@@ -0,0 +1,15 @@
+describe("login view", () => {
+  it("it should display an error message when inputs are not valid", async () => {
+
+    expect(true).toBe(true)
+  });
+
+  it("it should display a success message when inputs are valid", async () => {
+    expect(true).toBe(true)
+  });
+
+  it("it should display an error message if the email address is invalid", () => {
+    // TODO: implement this test
+    expect(true).toBe(true)
+  });
+});
